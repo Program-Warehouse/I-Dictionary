91e4e86e6f784c79c555be045fc1ea25603baa98
Varun Sharma <coolvarunsharma117@gmail.com>
2020-10-19 03:16:08 +0530

    got the definition from every possible category(but displaying double ie. repeated, #needed to fix) #Varun_Sharma

--- app/src/main/java/com/example/i_dictionary/EnterWordOrSentenceActivity.java
+++ app/src/main/java/com/example/i_dictionary/EnterWordOrSentenceActivity.java
@@ -6,8 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,8 +16,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.jaeger.library.StatusBarUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,7 +30,6 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Objects;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -39,7 +42,11 @@ public class EnterWordOrSentenceActivity extends AppCompatActivity {
     Button b;
     Context context;
     SearchView enter_word_or_sentance_edittext_main;
-    static TextView t5, type;
+    static LinearLayout result_linear_layout;
+    static TextView result_word;
+
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -52,14 +59,16 @@ public class EnterWordOrSentenceActivity extends AppCompatActivity {
 
         TextView selected_language1 = findViewById(R.id.selected_language1);
         TextView selected_language2 = findViewById(R.id.selected_language2);
-        t5 = findViewById(R.id.textView5);
-        type = findViewById(R.id.textViewType);
+        result_linear_layout = findViewById(R.id.result_linear_layout);
+
+
+
 
         ImageView back_img = findViewById(R.id.back_button_img);
         back_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent back_button_img_intent = new Intent(EnterWordOrSentenceActivity.this, MainActivity.class);
+                Intent back_button_img_intent = new Intent(getApplicationContext(), MainActivity.class);
                 back_button_img_intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(back_button_img_intent);
                 finish();
@@ -83,24 +92,38 @@ public class EnterWordOrSentenceActivity extends AppCompatActivity {
                 });
                 anim.setDuration(250);
                 anim.start();
-                isNetworkStatusAvialable(EnterWordOrSentenceActivity.this);
+
+                result_linear_layout.removeAllViews();
                 url_entry = dictionaryEntries(s);
                 sendrequest();
-
                 return false;
             }
 
 
             @Override
             public boolean onQueryTextChange(String s) {
+                result_linear_layout.removeAllViews();
+
+                ValueAnimator anim = ValueAnimator.ofInt(enter_word_or_sentance_edittext_main.getMeasuredHeight(), 200);
+                anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        int val = (Integer) valueAnimator.getAnimatedValue();
+                        ViewGroup.LayoutParams layoutParams = enter_word_or_sentance_edittext_main.getLayoutParams();
+                        layoutParams.height = val;
+                        enter_word_or_sentance_edittext_main.setLayoutParams(layoutParams);
+                    }
+                });
+                anim.setDuration(250);
+                anim.start();
                 return false;
             }
+
         });
     }
 
 
     public void sendrequest(){
-        Log.v("clicked", "sendrequest_method");
         IDictionaryRequest iDictionaryRequest = new IDictionaryRequest(EnterWordOrSentenceActivity.this);
         iDictionaryRequest.execute(url_entry);
     }
@@ -121,18 +144,9 @@ public class EnterWordOrSentenceActivity extends AppCompatActivity {
         final String fields = "translations";
         final String strictMatch = "false";
         final String word_id = word.toLowerCase();
-        return "https://od-api.oxforddictionaries.com/api/v2/" + fields + "/" + l_source + "/" + l_target + "/" + word_id + "?strictMatch=" + strictMatch + "&fields=" + fields;
+        return "https://od-api.oxforddictionaries.com/api/v2/" + fields + "/" + l_source + "/" + l_target + "/" + word_id + "?strictMatch=" + strictMatch;
     }
-    public static boolean isNetworkStatusAvialable (Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivityManager != null)
-        {
-            NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
-            if(netInfos != null)
-                return netInfos.isConnected();
-        }
-        return false;
-    }
+
 
     public static class IDictionaryRequest extends AsyncTask<String, Integer, String> {
 
@@ -140,10 +154,12 @@ public class EnterWordOrSentenceActivity extends AppCompatActivity {
         final String app_key = "6896b290b8852c222903f50269063375";
         String myurl;
         Context context;
-        ArrayList<String> dr = new ArrayList<>();
+        int i=0;
+
 
 
         public IDictionaryRequest(Context context){
+            super();
             this.context = context;
         }
 
@@ -181,46 +197,77 @@ public class EnterWordOrSentenceActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             Log.v("jsonscript",s);
-            
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray results = jsonObject.getJSONArray("results");
+                JSONObject resultsJSONObject = results.getJSONObject(0);
 
-        try {
-            JSONObject jsonObject = new JSONObject(s);
-            JSONArray results = jsonObject.getJSONArray("results");
-            JSONObject resultsJSONObject = results.getJSONObject(0);
+                //result_word.setText(resultsJSONObject.getString("id"));
 
-            JSONArray lexicalEntries = resultsJSONObject.getJSONArray("lexicalEntries");
+                JSONArray lexicalEntries = resultsJSONObject.getJSONArray("lexicalEntries");
+                JSONObject lexicalEntriesJSONObject = null;
+                String type = "";
 
-            JSONObject lexicalEntriesJSONObject = lexicalEntries.getJSONObject(0);
 
-            JSONArray entries = lexicalEntriesJSONObject.getJSONArray("entries");
-            JSONObject entriesJSONObject = entries.getJSONObject(0);
-
-            JSONArray senses = entriesJSONObject.getJSONArray("senses");
-            JSONObject sensesJSONObject = senses.getJSONObject(0);
-
-            for(int j=0; j<5; j++){
-                JSONArray definitions = sensesJSONObject.getJSONArray("definitions");
-                dr.add(definitions.getString(0));
-
-                try{
-                    JSONArray subsenses = sensesJSONObject.getJSONArray("subsenses");
-                    JSONObject subsensesJSONObject = subsenses.getJSONObject(0);
-                    JSONArray subdefinitions = subsensesJSONObject.getJSONArray("definitions");
-                    JSONObject lexicalCategory = (JSONObject) lexicalEntriesJSONObject.get("lexicalCategory");
-                    Log.d("testing", "testing: " + lexicalCategory.get("text"));
-                    dr.add(subdefinitions.getString(0));
-                    StringBuilder r = new StringBuilder();
-                    for(int i = 0; i<dr.size(); i++){
-                        r.append(". ").append(dr.get(i));
+                for(int i=0; i<lexicalEntries.length();i++){
+                    lexicalEntriesJSONObject = lexicalEntries.getJSONObject(i);
+                    assert lexicalEntriesJSONObject != null;
+                    if(lexicalEntries.length()>0){
+                        JSONObject lexicalCategory = (JSONObject) lexicalEntriesJSONObject.get("lexicalCategory");
+                        if(lexicalCategory.get("id").equals("adjective")){
+                            type = "adj.";
+                        }
+                        else if(lexicalCategory.get("id").equals("interjection")){
+                            type = "intj.";
+                        }
+                        else if(lexicalCategory.get("id").equals("noun")){
+                            type = "nou.";
+                        }
+                        else if(lexicalCategory.get("id").equals("adverb")){
+                            type = "adv.";
+                        }
+                        else if(lexicalCategory.get("id").equals("pronoun")){
+                            type = "pnou.";
+                        }
+                        else if(lexicalCategory.get("id").equals("verb")){
+                            type = "verb.";
+                        }
+                        else {
+                            type = "-";
+                        }
                     }
-                    type.setText(String.valueOf(lexicalCategory.get("text")));
-                    t5.setText(r);
+                    JSONArray entries = lexicalEntriesJSONObject.getJSONArray("entries");
+                    JSONObject entriesJSONObject = entries.getJSONObject(0);
+                    JSONArray senses = entriesJSONObject.getJSONArray("senses");
+
+                    for(int j=0; j<senses.length(); j++){
+                        JSONObject sensesJSONObject = senses.getJSONObject(j);
+                        JSONArray definitions = sensesJSONObject.getJSONArray("definitions");
+                        LinearLayout parent = new LinearLayout(context);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        parent.setLayoutParams(layoutParams);
+                        parent.setOrientation(LinearLayout.HORIZONTAL);
+                        layoutParams.setMargins(0, 20, 0, 20);
+                        result_linear_layout.addView(parent);
+                        TextView tv = new TextView(context);
+                        tv.setTextColor(Color.BLACK);
+                        tv.setTextSize(15);
+                        tv.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
+                        TextView tv2 = new TextView(context);
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        params.setMargins(40,0,0,0);
+                        tv2.setLayoutParams(params);
+                        tv2.setTextColor(Color.BLACK);
+                        tv2.setTextSize(15);
+                        parent.addView(tv);
+                        parent.addView(tv2);
+                        tv2.setText(definitions.getString(0));
+                        tv.setText(type);
+                    }
                 }
-                catch (Exception e){}
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
         }
     }
