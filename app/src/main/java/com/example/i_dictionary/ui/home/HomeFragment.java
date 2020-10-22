c98a55bfbc0d49567465ba4ecc56afc91cd6f620
Sakshi Jain <sakshi.172015@gmail.com>
2020-10-22 11:45:43 +0530

    Ocr implemented

--- app/src/main/java/com/example/i_dictionary/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/i_dictionary/ui/home/HomeFragment.java
@@ -1,27 +1,26 @@
 package com.example.i_dictionary.ui.home;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.i_dictionary.EnterWordOrSentenceActivity;
-
-import com.example.i_dictionary.MainActivity;
+import com.example.i_dictionary.OcrActivity;
 import com.example.i_dictionary.R;
 
 public class HomeFragment extends Fragment {
 
 
     private HomeViewModel homeViewModel;
+    private ImageView camera;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +29,15 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         RelativeLayout enter_word_or_sentence_layout = root.findViewById(R.id.enter_word_or_sentance_layout);
         EditText enter_word_or_sentance_edittext = root.findViewById(R.id.enter_word_or_sentance_edittext);
+        camera = root.findViewById(R.id.camera_icon);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), OcrActivity.class));
+            }
+        });
+
         enter_word_or_sentance_edittext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
@@ -41,8 +49,5 @@ public class HomeFragment extends Fragment {
 
         return root;
     }
-
-
-
 
 }
